ff0a912e2aeae48fd56a0c604b115cf23c85f879
Pavel <shlyakpavel@gmail.com>
2019-02-14 13:14:00 +0300

    Hello darkness, my old friend

--- examples/u8g2_4wire_hw_spi/u8g2_4wire_hw_spi.c
+++ examples/u8g2_4wire_hw_spi/u8g2_4wire_hw_spi.c
@@ -18,7 +18,7 @@ int main(void)
     //u8g2_Setup_ssd1306_128x64_noname_f(&u8g2, U8G2_R0, u8x8_byte_arm_linux_hw_spi, u8x8_arm_linux_gpio_and_delay);
     //u8x8_SetPin(u8g2_GetU8x8(&u8g2), U8X8_PIN_DC, OLED_SPI_PIN_DC);
     //u8x8_SetPin(u8g2_GetU8x8(&u8g2), U8X8_PIN_RESET, OLED_SPI_PIN_RES);
-    //u8x8_SetPin(u8g2_GetU8x8(&u8g2), U8X8_PIN_CS, OLED_SPI_PIN_CS);
+    u8x8_SetPin(u8g2_GetU8x8(&u8g2), U8X8_PIN_CS, OLED_SPI_PIN_CS);
 
     u8g2_InitDisplay(&u8g2);
     u8g2_SetPowerSave(&u8g2, 0);
